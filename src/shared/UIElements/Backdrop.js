879e86f68549f4d879f8bdf986c729c3b679d4a1
LearnToCode1138 <mdelgado1138@hotmail.com>
2023-05-29 20:57:58 -0700

    UI elements misc

---
+++ src/shared/UIElements/Backdrop.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import classes from './Backdrop.module.css';
+
+const Backdrop = (props) => {
+  return ReactDOM.createPortal(
+    <div className={classes.backdrop} onClick={props.onClick}></div>,
+    document.getElementById('backdrop-hook')
+  );
+};
