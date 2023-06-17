9afbd51cc9c043122c22d4d4990898b0cd838028
LearnToCode1138 <mdelgado1138@hotmail.com>
2023-06-16 20:47:51 -0700

    catalog store

---
+++ src/store/catalog-slice.js
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const catalogSlice = createSlice({
+  name: 'catalog',
+  initialState: {
+    products: [],
+    changed: false,
+  },
+  reducers: {
+    replaceCatalog(state, action) {
+      state.products = action.payload.products;
+    },
+    setCatalogStorage(state) {},
+    getCatalogStorage(state) {},
+    getProduct(state, action) {},
+    addProduct(state, action) {},
+    updateExistingProduct(state, action) {},
+    toggleProductActive(state, action) {},
+    deleteProduct(state, action) {},
+    addSubscriber(state, action) {},
+    removeSubscriber(state, action) {},
+  },
+});
+
+export const catalogActions = catalogSlice.actions;
+export default catalogSlice;
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import authSlice from './auth-slice';
+import catalogSlice from './catalog-slice';
 
 const store = configureStore({
   reducer: {
     auth: authSlice.reducer,
+    catalog: catalogSlice.reducer,
   },
 });
 
